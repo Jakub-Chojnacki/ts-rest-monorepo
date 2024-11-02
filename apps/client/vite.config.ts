a106d28cdafab5f5d13aac257a1c939f38f5e5f5
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-11-02 18:02:03 +0100

    initial setup of monorepo, pre-contract config

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import commonjs from "@rollup/plugin-commonjs";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [commonjs()],
+    },
+  },
+  optimizeDeps: {
+    include: ["api-contract/**/*"],
+  },
+});
