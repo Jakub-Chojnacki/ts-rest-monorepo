57bcef5508c816fd6c4e999e5debe22a3f5255f0
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-11-03 12:45:10 +0100

    setup contract

---
+++ apps/client/src/api-client.ts
@@ -0,0 +1,9 @@
+import { initQueryClient } from "@ts-rest/react-query";
+import { contract } from "api-contract";
+
+export const apiClient = initQueryClient(contract, {
+  baseHeaders: {},
+  baseUrl: "",
+});
+
+export default apiClient;
