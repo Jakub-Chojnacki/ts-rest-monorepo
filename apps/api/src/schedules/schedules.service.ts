eae4a19127de64e224f50898ca49f13686a56f5a
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-11-30 16:49:55 +0100

    added optio to create and edit schedules

--- apps/api/src/schedules/schedules.service.ts
+++ apps/api/src/schedules/schedules.service.ts
@@ -1,48 +1,93 @@
 import z from 'zod';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 import { PrismaService } from 'src/prisma.service';
 import { contract } from 'api-contract';
 
 type TSchedule = z.infer<typeof contract.schedules.create.body>;
-
 @Injectable()
 export class SchedulesService {
   constructor(private prisma: PrismaService) {}
-
   async create(schedule: Omit<TSchedule, 'id'>) {
     try {
-      const newSchedule = await this.prisma.schedule.create({
-        data: {
-          duration: schedule.duration,
-          start: schedule.start,
-          end: schedule.end,
-          daysOfWeek: schedule.daysOfWeek,
-          userId: schedule.userId,
-        },
+      const result = await this.prisma.$transaction(async (prisma) => {
+        const newSchedule = await prisma.schedule.create({
+          data: {
+            eventDuration: schedule.eventDuration,
+            userId: schedule.userId,
+          },
+        });
+
+        const dailyTimings = [
+          {
+            dayOfWeek: 'MONDAY',
+            start: new Date('2023-01-01T09:00:00'),
+            end: new Date('2023-01-01T10:00:00'),
+            scheduleId: newSchedule.id,
+          },
+          {
+            dayOfWeek: 'TUESDAY',
+            start: new Date('2023-01-01T08:00:00'),
+            end: new Date('2023-01-01T16:30:00'),
+            scheduleId: newSchedule.id,
+          },
+        ];
+
+        const createdDailyTimings =
+          await prisma.dailyTiming.createManyAndReturn({
+            data: dailyTimings as TSchedule['dailyTimings'],
+          });
+
+        const finalSchedule = await prisma.schedule.update({
+          where: { id: newSchedule.id },
+          data: { dailyTimings: { connect: createdDailyTimings } },
+          include: { dailyTimings: true },
+        });
+
+        return finalSchedule;
       });
 
-      return newSchedule;
+      return result;
     } catch (error) {
-      //TODO: Add error handling
+      if (error instanceof z.ZodError) {
+        throw new BadRequestException('Invalid schedule data provided');
+      }
+
+      throw new InternalServerErrorException('Failed to create schedule');
     }
   }
 
-  edit(scheduleId: string, schedule: Omit<TSchedule, 'id'>) {
+  async edit(scheduleId: string, schedule: Partial<Omit<TSchedule, 'id'>>) {
     try {
       const updatedSchedule = this.prisma.schedule.update({
         where: { id: scheduleId },
         data: {
-          duration: schedule.duration,
-          start: schedule.start,
-          end: schedule.end,
-          daysOfWeek: schedule.daysOfWeek,
+          eventDuration: schedule?.eventDuration,
+          userId: schedule?.userId,
+          dailyTimings: schedule?.dailyTimings
+            ? {
+                upsert: schedule.dailyTimings.map((timing) => ({
+                  where: { id: timing.id },
+                  update: timing,
+                  create: timing,
+                })),
+              }
+            : undefined,
         },
+        include: { dailyTimings: true },
       });
 
       return updatedSchedule;
     } catch (error) {
-      //TODO: Add error handling
+      if (error instanceof z.ZodError) {
+        throw new BadRequestException('Invalid schedule data provided');
+      }
+
+      throw new InternalServerErrorException('Failed to create schedule');
     }
   }
 }
--- packages/api-client/src/schemas/schedules.ts
+++ packages/api-client/src/schemas/schedules.ts
@@ -1,14 +1,32 @@
 import z from "zod";
 
-const daysOfWeek = z.enum(["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]);
+const daysOfWeek = z.enum([
+  "MONDAY",
+  "TUESDAY",
+  "WEDNESDAY",
+  "THURSDAY",
+  "FRIDAY",
+  "SATURDAY",
+  "SUNDAY",
+]);
+
+const DailyTimingSchema = z.object({
+  id: z.string(),
+  dayOfWeek: daysOfWeek,
+  start: z.date(),
+  end: z.date(),
+  scheduleId: z.string(),
+});
 
 export const ScheduleSchema = z.object({
-    id: z.string(),
-    start: z.date(),
-    end: z.date(),
-    userId: z.string(),
-    duration: z.number(),
-    daysOfWeek: z.array(daysOfWeek),
-})
+  id: z.string(),
+  dailyTimings: z.array(DailyTimingSchema),
+  eventDuration: z.number(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  userId: z.string(),
+});
 
 export type TSchedule = z.infer<typeof ScheduleSchema>;
+
+export type TDailyTiming = z.infer<typeof DailyTimingSchema>;
