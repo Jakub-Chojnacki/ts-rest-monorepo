e93ccaa8368e066e21bbf94b6b424d9919e3f8d7
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-12-18 21:58:54 +0100

    Remove vuetify, add vue-shadcn, add vue-full-calendar, add basic components from vue-shadcn

--- apps/vue-client/src/main.ts
+++ apps/vue-client/src/main.ts
@@ -1,9 +1,6 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 import { createApp } from "vue";
-import { createVuetify } from "vuetify";
 import { createPinia } from "pinia";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
 import App from "./App.vue";
 import HelloWorld from "./components/HelloWorld.vue";
@@ -12,11 +9,6 @@ import "./style.css";
 
 const pinia = createPinia();
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-
 const routes = [{ path: "/", component: HelloWorld }];
 
 const router = createRouter({
@@ -24,4 +16,4 @@ const router = createRouter({
   routes,
 });
 
-createApp(App).use(pinia).use(vuetify).use(router).mount("#app");
+createApp(App).use(pinia).use(router).mount("#app");
