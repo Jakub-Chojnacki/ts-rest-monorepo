510e26a5dca81b0a34a25657911c15621af8c8e8
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2025-04-05 20:00:22 +0200

    feat: add getUserById to fix auth/me

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -11,7 +11,25 @@ import { PrismaService } from 'src/prisma.service';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async findOne(id: string): Promise<User | undefined> {
+  async findOne(username: string): Promise<User | undefined> {
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: { username },
+      });
+
+      if (!user) {
+        throw new NotFoundException(`User not found`);
+      }
+
+      return user;
+    } catch (error) {
+      console.error(`Error finding user`, error);
+
+      throw new InternalServerErrorException('Error retrieving user');
+    }
+  }
+
+  async findById(id: string): Promise<User | null> {
     try {
       const user = await this.prisma.user.findFirst({
         where: { id },
